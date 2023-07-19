77e5d84898130f536d813ae279e8b0d1082da4f1
Bardia Khademi <138980378+BardiaKhd@users.noreply.github.com>
2023-07-19 15:21:32 +0330

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(Application());
+}
+
+class Application extends StatelessWidget {
+  const Application({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return getApplication();
+  }
+}
+
+Widget getApplication() {
+  return MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.amber,
+      body: SafeArea(
+        child: DecoratedBox(
+          position: DecorationPosition.background,
+          decoration: BoxDecoration(
+            color: Colors.amber,
+            image: DecorationImage(
+              image: AssetImage('images/drum.png')
+            ), 
+          ),
+          child: getBody(),
+        ),
+      ),
+    ),
+  );
+}
+
+Widget getBody() {
+  return Column(
+    children: [
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(child: TextButton(
+              onPressed: () {
+                playSound('h1.wav');
+              }, 
+              child: Text(''),
+              ),
+            ),
+            Expanded(child: TextButton(
+              onPressed: () {
+                playSound('h2.wav');
+              }, 
+              child: Text(''),
+              ),
+            ),   
+          ],
+        ),
+      ),
+
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(child: TextButton(
+              onPressed: () {
+                playSound('c1.wav');
+              },
+              child: Text(''),
+              ),
+            ),
+            Expanded(child: TextButton(
+              onPressed: () {
+                playSound('c2.wav');
+              }, 
+              child: Text(''),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(child: TextButton(
+              onPressed: () {
+                playSound('k1.wav');
+              },
+              child: Text(''))),
+            Expanded(child: TextButton(
+              onPressed: () {
+                playSound('k2.wav');
+              }, 
+              child: Text(''),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+playSound(String sound) {
+  var player = AudioCache();
+     player.play(sound);
+}
